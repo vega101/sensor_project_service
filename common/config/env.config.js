e2a4397e248adc47fa6754a8d26854474b43b58f
Ray Turp <rayturp@gmail.com>
2020-07-14 20:36:14 +0100

    updated port to 8000

--- common/config/env.config.js
+++ common/config/env.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    "port": 80, //3600
-    "appEndpoint": "http://0.0.0.0:80", //http://localhost:3600
-    "apiEndpoint": "http://0.0.0.0:80", //http://localhost:3600
+    "port": 8000, //3600
+    "appEndpoint": "http://0.0.0.0:8000", //http://localhost:3600
+    "apiEndpoint": "http://0.0.0.0:8000", //http://localhost:3600
     "jwt_secret": "myS33!!creeeT",
     "jwt_expiration_in_seconds": 36000,
     "environment": "dev",
